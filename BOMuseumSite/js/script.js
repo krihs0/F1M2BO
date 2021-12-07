174aef51144cfc10ee0e20ac3464f1a0eb0ff57c
Krins <32813@ma-web.nl>
2021-12-07 10:11:32 +0100

    site update

---
+++ BOMuseumSite/js/script.js
@@ -0,0 +1,17 @@
+let lang = "NL"
+
+function onClickNL(){
+    lang = "NL"
+    setLang()
+}
+
+function onClickUK(){
+    lang = "EN"
+    setLang()
+}
+
+function setLang(){
+    console.log("setLang function in development")
+    //set lang tag/change page
+    console.log(lang)
+}
